3c5a2cc9ec342bead20e728e8a6c406403a6fe15
a23310173 <a23310173@ceti.mx>
2025-05-12 22:24:44 -0600

    Creacion de la vista del administrado y modificaciones esteticas al login y registro de usuarios.

---
+++ vista_administrador.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel de Administración</title>
+    <link rel="stylesheet" href="vista_administrador.css">
+</head>
+<body>
+
+<div class="title-container">
+    <img src="media/tempologo.png" alt="Logo" class="logo">
+    <h1>Panel de Administración</h1>
+</div>
+
+<div class="admin-container">
+    <a href="administrar_usuarios.php" class="admin-btn">Administrar Usuarios</a>
+    <a href="productos_admini.php" class="admin-btn">Administrar Productos</a>
+    <a href="registro_productos.html" class="admin-btn">Registrar Producto</a>
+    <a href="bitacora.php" class="admin-btn">Bitácora</a>
+</div>
+
+<div class="contenedor-btn-volver">
+    <a href="home.html" class="btn-volver">← Volver a la página de inicio</a>
+</div>
+
+</body>
+</html>
+<?php
